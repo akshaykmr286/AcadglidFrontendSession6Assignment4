9df43eb67c45a65b4a59ac556d6c6bb3942c6271
Akshay Kumar Nayak <akshaykmr286@gmail.com>
2017-06-27 11:29:02 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,11 +8,23 @@ function create_Row() {
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "NEW CELL1";
     cell2.innerHTML = "NEW CELL2";
+    disp();
 }
-
 /*
 This function is called when the Delete Row Button Is clicked and it Deletes a row
 */
 function deleteRow() {
     document.getElementById("dynamicTable").deleteRow(0);
+    disp();
+}
+
+/*
+display current rows and columns
+*/
+function disp(){
+     var table = document.getElementById("dynamicTable");
+    var noOfRows=table.rows.length; 
+   var noOfCols=table.rows[0].cells.length;
+   document.getElementById("rowss").innerHTML=noOfRows; 
+   document.getElementById("colss").innerHTML=noOfCols; //alert(document.getElementById('dynamicTable').rows.length);
 }
